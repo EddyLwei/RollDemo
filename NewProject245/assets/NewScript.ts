21c65c52eb92b83e30d661cfafa93bdb22a806fe
erniu <2485880454@qq.com>
2022-10-09 16:45:18 +0800

    【测试】352提交多测试

--- NewProject245/assets/NewScript.ts
+++ NewProject245/assets/NewScript.ts
@@ -31,57 +31,72 @@ export default class NewClass extends cc.Component {
 
 
 
+    _state: boolean = false;
+    m_BgSpeed: number = 1000;
 
     start() {
-
+        // cc.game.setFrameRate(45);
     }
 
-    update(deltaTime: number) {
+    update(dt: number) {
 
-        if (Math.abs(this.headParent.x) >= this._nowP) {
-            this._showImg1 = !this._showImg1;
-            let img = this.head1;
-            if (this._showImg1) {
-                img = this.head2;
+        if (this._state) {
+
+            this.head1.x -= dt * this.m_BgSpeed; this.head2.x -= dt * this.m_BgSpeed;
+            if (this.head1.x < -this.head1.width) {
+                this.head1.x = this.head1.width - dt * this.m_BgSpeed;
             }
-            this._nowP = img.x + this._itemWidth * 2;
-            // img.setPosition(new Vec3(this._nowP, 0, 0));
-            img.x = this._nowP;
+            if (this.head2.x < -this.head2.width) {
+                this.head2.x = this.head2.width - dt * this.m_BgSpeed;
+            }
         }
+
+        // if (Math.abs(this.headParent.x) >= this._nowP) {
+        //     this._showImg1 = !this._showImg1;
+        //     let img = this.head1;
+        //     if (this._showImg1) {
+        //         img = this.head2;
+        //     }
+        //     this._nowP = img.x + this._itemWidth * 2;
+        //     // img.setPosition(new Vec3(this._nowP, 0, 0));
+        //     img.x = this._nowP;
+        // }
     }
 
 
     clickStart() {
-        this._showImg1 = true;
-        // this.head1.setPosition(new Vec3(0, 0, 0))
-        // this.head2.setPosition(new Vec3(this._itemWidth, 0, 0))
-        this.head1.x = 0;
-        this.head2.x = this._itemWidth;
-        this._nowP = this._itemWidth;
-        //目标在第一百个，居中开始表示左侧有一半不需要滚动9/2
-        let headNun = 100 - Math.ceil(9 / 2);
-        let targetPosX = -this._itemHeadWidth * (headNun);  //计算停止位置
-        this._targetX = -this._itemHeadWidth * (headNun);  //计算停止位置
+        this._state = !this._state;
+        console.log("clickStart========", this._state)
+        // this._showImg1 = true;
+        // // this.head1.setPosition(new Vec3(0, 0, 0))
+        // // this.head2.setPosition(new Vec3(this._itemWidth, 0, 0))
+        // this.head1.x = 0;
+        // this.head2.x = this._itemWidth;
+        // this._nowP = this._itemWidth;
+        // //目标在第一百个，居中开始表示左侧有一半不需要滚动9/2
+        // let headNun = 100 - Math.ceil(9 / 2);
+        // let targetPosX = -this._itemHeadWidth * (headNun);  //计算停止位置
+        // this._targetX = -this._itemHeadWidth * (headNun);  //计算停止位置
 
-        let spinTime = 15;
+        // let spinTime = 15;
 
-        console.warn(targetPosX, " initPox>> ", -this._itemHeadWidth * (headNun), " spinTime>> ", spinTime, " headNun>> ", headNun, "------滚动 开始->");
+        // console.warn(targetPosX, " initPox>> ", -this._itemHeadWidth * (headNun), " spinTime>> ", spinTime, " headNun>> ", headNun, "------滚动 开始->");
 
-        this.headParent.x = 0;
+        // this.headParent.x = 0;
 
-        cc.Tween.stopAll();
-        this.txt.string = "滚动中 2.45";
-        let self = this;
-        cc.tween(this.headParent)//, { easing: "fade" }//, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) }
-            // .to(spinTime, { position: cc.v3(targetPosX, 0), }, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) })
-            // .to(spinTime, { position: new Vec3(targetPosX, 0, 0) }, { easing: t => t * (2 - t) })
-            .to(spinTime, { position: cc.v3(targetPosX, 0, 0) }, { easing: "fade" })
-            .call(() => {
-                // self.spinFinish();
-                console.log("结束》》》》》")
-                this.txt.string = "结束 2.45";
-            })
-            .start();
+        // cc.Tween.stopAll();
+        // this.txt.string = "滚动中 2.45";
+        // let self = this;
+        // cc.tween(this.headParent)//, { easing: "fade" }//, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) }
+        //     // .to(spinTime, { position: cc.v3(targetPosX, 0), }, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) })
+        //     // .to(spinTime, { position: new Vec3(targetPosX, 0, 0) }, { easing: t => t * (2 - t) })
+        //     .to(spinTime, { position: cc.v3(targetPosX, 0, 0) }, { easing: "fade" })
+        //     .call(() => {
+        //         // self.spinFinish();
+        //         console.log("结束》》》》》")
+        //         this.txt.string = "结束 2.45";
+        //     })
+        //     .start();
     }
 
 }
--- NewProject352/assets/MainSc.ts
+++ NewProject352/assets/MainSc.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, bezierByTime, tween, Vec3, Label, Tween } from 'cc';
+import { _decorator, Component, Node, bezierByTime, tween, Vec3, Label, Tween, ScrollView } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('MainSc')
@@ -16,6 +16,15 @@ export class MainSc extends Component {
     @property({ type: Node, tooltip: "滚动列表容器" })
     head2: Node = null;
 
+
+
+
+    @property({ type: ScrollView, tooltip: "滚动" })
+    scView: ScrollView = null;
+
+
+
+
     _itemHeadWidth: number = 75;
     _targetX: number = 0;
 
@@ -30,25 +39,27 @@ export class MainSc extends Component {
 
     }
 
-    update(deltaTime: number) {
+    // update(deltaTime: number) {
 
-        if (Math.abs(this.headParent.position.x) >= this._nowP) {
-            this._showImg1 = !this._showImg1;
-            let img = this.head1;
-            if (this._showImg1) {
-                img = this.head2;
-            }
-            this._nowP = img.position.x + this._itemWidth * 2;
-            img.setPosition(new Vec3(this._nowP, 0, 0));
-        }
-    }
+    //     if (Math.abs(this.headParent.position.x) >= this._nowP) {
+    //         this._showImg1 = !this._showImg1;
+    //         let img = this.head1;
+    //         if (this._showImg1) {
+    //             img = this.head2;
+    //         }
+    //         this._nowP = img.position.x + this._itemWidth * 2;
+    //         img.setPosition(new Vec3(this._nowP, 0, 0));
+    //     }
+    // }
 
 
     clickStart() {
-        this._showImg1 = true;
-        this._nowP = 750;
-        this.head1.setPosition(new Vec3(0, 0, 0))
-        this.head2.setPosition(new Vec3(this._itemWidth, 0, 0))
+        this._nowState = 0;
+        this.scView.node.active = false;
+        // this._showImg1 = true;
+        // this._nowP = 750;
+        // this.head1.setPosition(new Vec3(0, 0, 0))
+        // this.head2.setPosition(new Vec3(this._itemWidth, 0, 0))
         //目标在第一百个，居中开始表示左侧有一半不需要滚动9/2
         let headNun = 100 - Math.ceil(9 / 2);
         let targetPosX = -this._itemHeadWidth * (headNun);  //计算停止位置
@@ -58,10 +69,9 @@ export class MainSc extends Component {
 
         console.warn(targetPosX, " initPox>> ", -this._itemHeadWidth * (headNun), " spinTime>> ", spinTime, " headNun>> ", headNun, "------滚动 开始->");
 
-        this.headParent.setPosition(new Vec3(0, 0, 0));
-
         Tween.stopAll();
-        this.txt.string = "滚动中 3.5.2";
+        this.headParent.setPosition(new Vec3(0, 0, 0));
+        this.txt.string = "tween 滚动中 3.5.2";
         let self = this;
         tween(this.headParent)//, { easing: "fade" }//, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) }
             .to(spinTime, { position: new Vec3(targetPosX, 0, 0) }, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) })
@@ -76,113 +86,78 @@ export class MainSc extends Component {
     }
 
 
+    clickTest2() {
+        this.scView.node.active = false;
+        console.log("-滚动---clickTest2----------")
+        Tween.stopAll();
+        this.headParent.setPosition(new Vec3(0, 0, 0));
+        this.txt.string = "坐标 滚动中 3.5.2";
+        this._timePro = 0;
+        let headNun = 100 - Math.ceil(9 / 2);
+        this._targetX = -this._itemHeadWidth * (headNun);  //计算停止位置
+
+        this._nowState = 1;
+    }
 
 
 
 
+    /**时间进度*/
+    _timePro: number = 0;
+
+    /**总时间*/
+    _allPro: number = 12;
+
+    _nowState: number = 0;
+
+
+    _intervalTime: number = 0;
+
+    update(dt) {
+
+        //滚动逻辑
+        if (this._nowState == 1) {
+            this._intervalTime += dt;
+
+            if (this._intervalTime > 0.01) {
+                // console.log(this.headParent.position.x, " ==?== ", this._showSpinItemIndex, (this._showSpinItemIndex - 2) * -this._itemWidth)
+                this._timePro += 0.01;
+                let x = this._timePro / this._allPro;
+
+                this._intervalTime = 0;
+
+                // let posx = (bezierByTime([0.3, 0, 0, 1], x)) * this._targetX;
+                let posx = x * this._targetX;
+                this.headParent.setPosition(new Vec3(posx, 0, 0));
+
+                if (x >= 1) {
+                    this.txt.string = "结束 3.5.2";
+                    this._nowState = 0;
+                }
+                // else {
+                //     console.log(this._targetX, " = ", x, " = ", bezierByTime([0.3, 0, 0, 1], x), "--posx", posx);
+                // }
+
+
+            }
+        }
+    }
 
 
 
 
+    _to: boolean = true;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // @property({ type: Node, tooltip: "滚动玩家头像" })
-    // rollPlayerItem: Node = null;
-    // _headArr: Array<Node> = [];
-    // testSpin() {
-
-    //     this.spinNode.active = true;
-    //     this._nowState = GameState.SPIN;
-    //     GameData.INIT_DATA = true;
-    //     //目标在第一百个，居中开始表示左侧有一半不需要滚动9/2
-    //     let headNun = 100 - Math.ceil(9 / 2);
-    //     let targetPosX = -this._itemHeadWidth * (headNun);  //计算停止位置
-    //     this._targetX = -this._itemHeadWidth * (headNun);  //计算停止位置
-
-    //     let spinTime = 15;
-
-    //     Log.warn(targetPosX, " initPox>> ", -this._itemHeadWidth * (headNun), " spinTime>> ", spinTime, " headNun>> ", headNun, "------滚动 开始->", GameData.ResultClientIndexList[99], GameData.ResultClientIndexList);
-
-    //     this._showSpinItemIndex = 0;
-    //     this.headParent.setPosition(new Vec3(0, 0, 0));
-
-    //     for (let index = 0; index < this._showSpinItemArr.length; index++) {
-    //         this._showSpinItemArr[index].setDataIndex(this._showSpinItemIndex);
-    //         this._showSpinItemIndex++;
-    //     }
-    //     this._timePro = 0;
-    //     let self = this;
-    //     tween(this.headParent)//, { easing: "fade" }//, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) }
-    //         .to(spinTime, { position: new Vec3(targetPosX, 0, 0) }, { easing: t => (bezierByTime([0.3, 0, 0, 1], t)) })
-    //         // .to(spinTime, { position: new Vec3(targetPosX, 0, 0) }, { easing: t => t * (2 - t) })
-    //         // .to(spinTime, { position: new Vec3(targetPosX, 0, 0) }, { easing: "fade" })
-    //         .call(() => {
-    //             // self.spinFinish();
-    //             Log.log("结束》》》》》")
-    //         })
-    //         .start();
-    // }
-
-    // _targetX: number = 0;
-
-    // /**时间进度*/
-    // _timePro: number = 0;
-
-    // /**总时间*/
-    // _allPro: number = 12;
-
-    // update(dt) {
-
-    //     //数据还没初始化
-    //     if (!GameData.INIT_DATA) {
-    //         return;
-    //     }
-
-    //     //页面UI还没初始化完成
-    //     if (this._initUI < 2) {
-    //         return;
-    //     }
-
-    //     //滚动逻辑
-    //     if (this._nowState == GameState.SPIN) {
-    //         // Log.log(this.headParent.position.x, " ==?== ", this._showSpinItemIndex, (this._showSpinItemIndex - 2) * -this._itemWidth)
-
-    //         // // if (this.headParent.position.x > this._targetX) {
-    //         // this._timePro += dt;
-    //         // let x = this._timePro / this._allPro;
-    //         // let posx = (bezierByTime([0.3, 0, 0, 1], x)) * this._targetX;
-    //         // this.headParent.setPosition(new Vec3(posx, 0, 0));
-
-    //         // if (x >= 1) {
-    //         //     this._nowState = GameState.REWARD;
-    //         // }
-    //         // else {
-    //         //     Log.log(this._targetX, " = ", bezierByTime([0.3, 0, 0, 1], x), "--posx", posx);
-    //         // }
-
-    //         // if (this.headParent.position.x < (this._showSpinItemIndex - 2) * -this._itemHeadWidth * this._itemHeadNum) {
-    //         //     this.moveItem2End();
-    //         // }
-    //         // }
-    //     }
-
-
-    // }
-
-
+    clickTest3() {
+        this.scView.node.active = true;
+        this._to = !this._to;
+        if (this._to) {
+            this.scView.scrollToLeft(this._allPro + 5);
+        }
+        else {
+            this.scView.scrollToRight(this._allPro + 5);
+        }
+    }
 
 }
 
